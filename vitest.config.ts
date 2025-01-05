2226df052ba3a44d108ef24405805601f041a80d
Karol <kjastrzebowski@auvik.com>
2025-01-05 17:04:49 +0100

    feat: add vitests coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,29 @@
-// w/o this file VSC vitest extension crushes ...
-const config = {}
-export default config
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['text'],
+      include: ['src/**'],
+      exclude: [
+        'node_modules/',
+        'dist/',
+        '**/*.d.ts',
+        'test/**',
+        // We don't collect coverage for test files
+        '**/*.{test,spec}.{js,ts}',
+        // We don't collect coverage for index files
+        '**/index.{js,ts}',
+      ],
+      all: true,
+      thresholds: {
+        // TODO rais all up to 98
+        lines: 96,
+        functions: 98,
+        branches: 95,
+        statements: 96
+      }
+    },
+  },
+})
